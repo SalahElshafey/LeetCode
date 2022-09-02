17a6cd03ca1ef0578408bffb5dc3a324dbb64ddb
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2022-09-03 01:18:38 +0200

    Create 50. Pow(x, n).cpp

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double y=1;
+        if(n>0){
+       y=pow(x,n);
+        return y;}else{
+           y=pow(x,n);
+            return y;
+        }
+        
+        
+    }
+};
